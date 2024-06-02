6e6aea6fcd1d67c9e5d37171a14e620ed1a8ef71
Guillaume Lecêtre <guillaume.lecetre@aworldforus.com>
2024-06-02 09:50:57 +0200

    wip

---
+++ src/components/List.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export type ListProps = {
+  children: ReactNode | ReactNode[];
+};
+
+export function List(props: ListProps) {
+  return <ul>{props.children}</ul>;
+}
---
+++ src/data/cosmogonies.ts
@@ -0,0 +1,26 @@
+import { eq } from "drizzle-orm";
+import { database } from "@/data/database";
+import { SelectCosmogony, cosmogoniesTable } from "@/data/schema";
+
+/**
+ * Get all the cosmogonies.
+ * @returns All cosmogonies.
+ */
+export async function getAllCosmogonies() {
+  return database.select().from(cosmogoniesTable);
+}
+
+/**
+ * Get a single cosmogony by its slug.
+ * @param slug Slug of the cosmogony to fetch.
+ * @returns The cosmogony.
+ */
+export async function getCosmogonyBySlug(slug: SelectCosmogony["slug"]) {
+  const result = await database
+    .select()
+    .from(cosmogoniesTable)
+    .where(eq(cosmogoniesTable.slug, slug))
+    .limit(1);
+
+  return result[0];
+}
